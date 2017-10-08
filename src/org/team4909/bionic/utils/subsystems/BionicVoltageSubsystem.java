2d8dcb435ccdefb65f60ea0d2345f179a1586697
Roshan Ravi <roshanravi10@gmail.com>
2017-10-07 21:49:50 -0400

    Enforce Motor Safety
    Set Motor Safety so All Voltage Based Motors are Disabled Unless
    Actively Driven. Simplifies code, in that we don’t have to disabled
    them ourselves, since the whileHeld command complicates this and also
    is good to have for actual safety in the event our code is crashing or
    the like

--- src/org/team4909/bionic/utils/subsystems/BionicVoltageSubsystem.java
+++ src/org/team4909/bionic/utils/subsystems/BionicVoltageSubsystem.java
@@ -3,6 +3,8 @@ package org.team4909.bionic.utils.subsystems;
 import org.team4909.bionic.utils.setpoints.VoltageSetpoint;
 
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.MotorSafety;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class BionicVoltageSubsystem extends Subsystem {
@@ -12,6 +14,8 @@ public class BionicVoltageSubsystem extends Subsystem {
 	
 	public BionicVoltageSubsystem(SpeedController speedController) {
 		this.speedController = speedController;
+		
+		((MotorSafety) this.speedController).setSafetyEnabled(true);
 	}
 	
 	public BionicVoltageSubsystem(SpeedController speedController, boolean inverted) {
