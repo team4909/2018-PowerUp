760e61240db1d7626729f0d658ea167c78913584
Roshan Ravi <roshanravi10@gmail.com>
2017-10-07 20:11:39 -0400

    Convert set Drive Direction to Instant Command

--- src/org/team4909/bionic/utils/commands/SetDriveDirection.java
+++ src/org/team4909/bionic/utils/commands/SetDriveDirection.java
@@ -1,25 +1,20 @@
 package org.team4909.bionic.utils.commands;
 
-import org.team4909.bionic.utils.core.BionicCommand;
 import org.team4909.bionic.utils.subsystems.BionicDrivetrain;
 import org.team4909.bionic.utils.subsystems.BionicDrivetrain.Direction;
 
-public class SetDriveDirection extends BionicCommand {
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+public class SetDriveDirection extends InstantCommand {
 	private BionicDrivetrain drivetrainSubsystem;
 	private Direction direction;
 	
 	public SetDriveDirection(BionicDrivetrain drivetrain, Direction direction) {
-		requires(drivetrain);
-		
 		drivetrainSubsystem = drivetrain;
 		this.direction = direction;
 	}
 	
-	protected void execute() {
+	protected void initialize() {
 		drivetrainSubsystem.driveDirection = direction;
     }
-
-	protected boolean isFinished() {
-		return true;
-	}
 }
