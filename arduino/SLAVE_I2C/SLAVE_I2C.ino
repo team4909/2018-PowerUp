c2cfa448f1387ec59190762cdff30069268eace6
Roshan Ravi <roshanravi10@gmail.com>
2017-11-04 15:55:11 -0400

    Update SLAVE_I2C.ino

--- arduino/SLAVE_I2C/SLAVE_I2C.ino
+++ arduino/SLAVE_I2C/SLAVE_I2C.ino
@@ -1,25 +1,26 @@
 #include <Wire.h>
-//Set PWM ports
+// Set PWM ports
 int RED = 9;
 int GREEN = 10;
 int BLUE = 11;
 
+// Set Configuration
 int lastValue = 0;
 int deviceNumber = 4;
 
 void setup() {
-  //Set Outputs (Needs to be modified to work with LED strip)
+  // Set Outputs (Needs to be modified to work with LED strip)
   pinMode(RED,OUTPUT);
   pinMode(GREEN,OUTPUT);
   pinMode(BLUE,OUTPUT);
 	
-  //Start I2C as slave
+  // Start I2C as slave
   Wire.begin(deviceNumber);
   Wire.onReceive(receiveEvent);
 }
 
 void receiveEvent(int bytes){
-  //Read the last number sent (0-255)
+  // Read the last number sent (0-255)
   lastValue = Wire.read();
 }
 
@@ -41,14 +42,14 @@ void loop() {
     case 4:
     	setColor(255,204,0);
    	break;
-    //ENABLE
+    // ENABLE
     case 6:
     	setColor(255, 25, 0);
     	delay(200);
     	setColor(0,0,0);
     	delay(200);
     	break;
-    //DISABLE
+    // DISABLE
     case 7:
     	setColor(255,25,0);
     	break;
@@ -59,8 +60,8 @@ void loop() {
 
 }
 void setColor(int red, int green, int blue){
-  //Write the color to the board
-  analogWrite(RED,red);
+  // Write the color to the board
+  analogWrite(RED, red);
   analogWrite(GREEN, green);
   analogWrite(BLUE, blue);
 }
