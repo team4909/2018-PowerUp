c8ba85352b8f91101f01d68cb81d2c52c87b6902
Roshan Ravi <roshanravi10@gmail.com>
2017-11-25 18:49:02 -0500

    Update SLAVE_I2C.ino
    Updated LED code based on Neopixel code received from @jaspercoughlin .
    
    Closes #43

--- arduino/SLAVE_I2C/SLAVE_I2C.ino
+++ arduino/SLAVE_I2C/SLAVE_I2C.ino
@@ -1,67 +1,64 @@
+#include <Adafruit_NeoPixel.h>
 #include <Wire.h>
-// Set PWM ports
-int RED = 9;
-int GREEN = 10;
-int BLUE = 11;
+
+#define PIXEL_PIN    6
+#define PIXEL_COUNT 16
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 // Set Configuration
 int lastValue = 0;
 int deviceNumber = 4;
 
 void setup() {
-  // Set Outputs (Needs to be modified to work with LED strip)
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
-	
   // Start I2C as slave
   Wire.begin(deviceNumber);
   Wire.onReceive(receiveEvent);
+
+  // Initialize LED Strip
+  strip.begin();
+  strip.show();
 }
 
-void receiveEvent(int bytes){
+void receiveEvent(int bytes) {
   // Read the last number sent (0-255)
   lastValue = Wire.read();
 }
 
 void loop() {
   //Add Pre-Programmed cases here:
-  switch(lastValue){
+  switch (lastValue) {
     case 0:
-    	setColor(0,255,0);
-    	break;
+      setColor(0, 255, 0);
+      break;
     case 1:
-    	setColor(255,0,0);
-	break;
+      setColor(255, 0, 0);
+      break;
     case 2:
-   	setColor(30,255,30);
-    	break;
+      setColor(30, 255, 30);
+      break;
     case 3:
-    	setColor(0,0,255);
-    	break;
+      setColor(0, 0, 255);
+      break;
     case 4:
-    	setColor(255,204,0);
-   	break;
+      setColor(255, 204, 0);
+      break;
     // ENABLE
     case 6:
-    	setColor(255, 25, 0);
-    	delay(200);
-    	setColor(0,0,0);
-    	delay(200);
-    	break;
+      setColor(255, 25, 0);
+      delay(200);
+      setColor(0, 0, 0);
+      delay(200);
+      break;
     // DISABLE
     case 7:
-    	setColor(255,25,0);
-    	break;
+      setColor(255, 25, 0);
+      break;
     default:
-    	setColor(0,255,0);
-    	break;
+      setColor(0, 255, 0);
+      break;
   }
-
 }
-void setColor(int red, int green, int blue){
-  // Write the color to the board
-  analogWrite(RED, red);
-  analogWrite(GREEN, green);
-  analogWrite(BLUE, blue);
+
+void setColor(int red, int green, int blue) {
+  colorWipe(strip.Color(red, green, blue), 50);
 }
