0c2ab8e40ca7923ca98f4933527904947c9ae7f6
Billerica Bionics <jasonbishop11@gmail.com>
2017-12-13 20:07:15 -0500

    Added code for arduino LEDs

--- src/org/team4909/preseason2018/core/Robot.java
+++ src/org/team4909/preseason2018/core/Robot.java
@@ -1,5 +1,6 @@
 package org.team4909.preseason2018.core;
 
+import org.team4909.bionic.utils.subsystems.Arduino;
 import org.team4909.bionic.utils.subsystems.BionicDrivetrain;
 import org.team4909.preseason2018.autonomous.AutonomousMap;
 
@@ -15,6 +16,8 @@ public class Robot extends IterativeRobot {
 	public static BionicDrivetrain drivetrain;
 	public static DoubleSolenoid flag;
 	
+	public static Arduino arduino;
+	
 	private void subsystemInit() {
 		drivetrain = new BionicDrivetrain(
 			new Spark(0), 
@@ -25,6 +28,7 @@ public class Robot extends IterativeRobot {
 		);
 		
 		flag = new DoubleSolenoid(0,1);
+		arduino = new Arduino(4);
 	}
 	
 	@Override
@@ -43,8 +47,17 @@ public class Robot extends IterativeRobot {
 		DashboardConfig.init();
 	}
 
-	@Override public void autonomousInit() { autonomousMap.startCommand(); }
-	@Override public void teleopInit() { autonomousMap.endCommand(); }
+	@Override public void autonomousInit() { 
+		arduino.sendData(6);
+		autonomousMap.startCommand(); 
+		}
+	
+	@Override public void teleopInit() {
+		arduino.sendData(6);	
+		autonomousMap.endCommand();
+		}
+	
+	@Override public void disabledInit() { arduino.sendData(7); }
 
 	@Override public void disabledPeriodic() { Scheduler.getInstance().run(); }
 	@Override public void autonomousPeriodic() { Scheduler.getInstance().run(); }
