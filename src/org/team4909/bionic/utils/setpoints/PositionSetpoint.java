827c3c0d4aff7e688a1de856c29c21ca7552473e
Roshan Ravi <roshanravi10@gmail.com>
2017-12-16 18:09:00 -0500

    Develop Position Subsystem

--- src/org/team4909/bionic/utils/setpoints/PositionSetpoint.java
+++ src/org/team4909/bionic/utils/setpoints/PositionSetpoint.java
@@ -1,13 +1,21 @@
 package org.team4909.bionic.utils.setpoints;
 
+import org.team4909.bionic.utils.subsystems.BionicPositionalSubsystem;
+
 public class PositionSetpoint {
+	private BionicPositionalSubsystem positionalSubsystem;
 	private double setpoint;
 	
-	public PositionSetpoint(double setpoint){
+	public PositionSetpoint(BionicPositionalSubsystem positionalSubsystem, double setpoint){
+		this.positionalSubsystem = positionalSubsystem;
 		this.setpoint = setpoint;	
 	}
 
-	public double get(){
+	public BionicPositionalSubsystem getSubsystem(){
+		return positionalSubsystem;
+	}
+	
+	public double getValue(){
 		return setpoint;
 	}	
 }
