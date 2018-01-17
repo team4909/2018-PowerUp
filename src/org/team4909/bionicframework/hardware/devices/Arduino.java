79a6e1d4027443b7bcf19b0e3a1d0a0678418df9
unknown <shashankjarmale1@gmail.com>
2018-01-16 20:50:44 -0500

    Allows For Increased Control Over LED Strips
    Code allows each of the 8 LED strips to be controlled individually, in terms of color using RGB values and on and off in general. The variables a, b, c, d, e, f, g, h, redValue, greenValue, abd blue Value are meant to be set by the user to determine the color and on and off state of each LED strip.

--- src/org/team4909/bionicframework/hardware/devices/Arduino.java
+++ src/org/team4909/bionicframework/hardware/devices/Arduino.java
@@ -4,10 +4,39 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
+int a = 0;  //Case 0 (First LED Strip)
+int b = 1;  //Case 1 (Second LED Strip)
+int c = 2;  //Case 2 (Third LED Strip)
+int d = 3;  //Case 3 (Fourth LED Strip)
+int e = 4;  //Case 4 (Fifth LED Strip)
+int f = 5;  //Case 5 (Sixth LED Strip)
+int g = 6;  //Case 6 (Seventh LED Strip)
+int h = 7;  //Case 7 (Eighth LED Strip)
+
+int redValue = 0;  //Red Value
+int greenValue = 255;  //Green Value
+int blueValue = 0;  //Blue Value
+
 public class Arduino {
 	 public static enum State {
-		 disabled(7),
-		 enabled(6);
+		 disabled(a),
+         disabled(b),
+         disabled(c),
+         disabled(d),
+         enabled(e),
+         enabled(f),
+         enabled(g),
+         enabled(h);
+         int disableArray = [a,b,c,d]  //Array of LEDs to turn off
+         int enableArray = [e,f,g,h]  //Array of LEDs to turn on
+             
+         public Command send(State state) {
+             case disableArray:
+               setColor(0,0,0); //Turn off all LEDs in disableArray
+             
+             case enableArray:
+               setColor(redValue,greenValue,blueValue);  //Set RGB value of LEDs in enableArrray
+         }
 		 
 		 public final int signal;
 		 State(int signal) {
