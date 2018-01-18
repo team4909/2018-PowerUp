42b233af1b853b19ab65f33a6c92414c72c347f3
Roshan Ravi <roshanravi10@gmail.com>
2018-01-17 20:19:14 -0500

    Update Comment

--- src/org/team4909/bionicframework/utils/Commandable.java
+++ src/org/team4909/bionicframework/utils/Commandable.java
@@ -3,7 +3,7 @@ package org.team4909.bionicframework.utils;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
 /**
- * InstantCommand that can be used either a function or as a handler for an operator action.
+ * InstantCommand that can be used directly or as a handler for an operator action.
  */
 public abstract class Commandable extends InstantCommand {
 	/** 
