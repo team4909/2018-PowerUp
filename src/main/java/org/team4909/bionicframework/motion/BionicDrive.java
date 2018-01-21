2144e7cc389961dc9a542ed824741f073510c948
Roshan Ravi <roshanravi10@gmail.com>
2018-01-21 12:36:02 -0500

    Cleanup Code

--- src/main/java/org/team4909/bionicframework/motion/BionicDrive.java
+++ src/main/java/org/team4909/bionicframework/motion/BionicDrive.java
@@ -21,150 +21,152 @@ import jaci.pathfinder.Waypoint;
  * BionicDrive abstracts away much of the underlying drivetrain functionality
  */
 public class BionicDrive extends Subsystem {
-	private enum DriveMode {
-		PercentOutput,
-		MotionProfile
-	};
-	
-	/* Internal State */
-	private DriveMode controlMode = DriveMode.PercentOutput;
-	private int profileInterval = 20;
-	
-	/* Hardware */
-	private final BionicSRX leftSRX;
-	private final BionicSRX rightSRX;
-	private final DifferentialDrive differentialDrive;
-	
-	/* OI */
-	private final BionicF310 speedInputGamepad;
-	private final BionicAxis speedInputAxis;
-	private final double speedScaleFactor = 1.0;
-	private final BionicF310 rotationInputGamepad;
-	private final BionicAxis rotationInputAxis;
-	private final double rotationScaleFactor = 1.0;
-	
-	/* Sensors */
-	private Gyro bionicGyro;
-//	private double gyro_p;
-	private PathgenUtil pathgen;
-	
-	/* Hardware Initialization */
-	/**
-	 * @param leftSRX Left Drivetrain SRX
-	 * @param rightSRX Right Drivetrain SRX
-	 * @param speedInputGamepad Speed Input Gamepad/Joystick
-	 * @param speedInputAxis Speed Input Axis
-	 * @param rotationInputGamepad Rotation Input Gamepad/Joystick
-	 * @param rotationInputAxis Rotation Input Axis
-	 * @param encoder Encoder type plugged into SRXs
-	 * @param encoder_p Proportional Constant in Encoder PID Controller
-	 * @param encoder_i Integral Constant in Encoder PID Controller
-	 * @param encoder_d Derivative Constant in Encoder PID Controller
-	 * @param bionicGyro Gyro to Use for Closed-Loop
-	 * @param gyro_p Proportional Constant in Gyro PID Controller
-	 * @param maxVelocity Max Velocity used for Path Generation
-	 * @param maxAccel Max Acceleration used for Path Generation
-	 * @param maxJerk Max Jerk used for Path Generation
-	 * @param drivebaseWidth Drivebase width between center of wheels
-	 * @param wheelDiameter Wheel diameter from end to end
-	 */
-	public BionicDrive(BionicSRX leftSRX, BionicSRX rightSRX,
-			BionicF310 speedInputGamepad, BionicAxis speedInputAxis,
-			BionicF310 rotationInputGamepad, BionicAxis rotationInputAxis,
-			FeedbackDevice encoder, double encoder_p, double encoder_i, double encoder_d,
-			Gyro bionicGyro, double gyro_p,
-			double maxVelocity, double maxAccel, double maxJerk,
-			double drivebaseWidth, double wheelDiameter) {
-		super();
-		
-		this.leftSRX = leftSRX;
-		this.rightSRX = rightSRX;
+    private enum DriveMode {
+        PercentOutput,
+        MotionProfile
+    };
 
-		this.leftSRX.enableVoltageCompensation(true);
-		this.rightSRX.enableVoltageCompensation(true);
+    /* Internal State */
+    private DriveMode controlMode = DriveMode.PercentOutput;
+    private int profileInterval = 20;
 
-		this.leftSRX.configSelectedFeedbackSensor(encoder);
-		this.rightSRX.configSelectedFeedbackSensor(encoder);
+    /* Hardware */
+    private final BionicSRX leftSRX;
+    private final BionicSRX rightSRX;
+    private final DifferentialDrive differentialDrive;
 
-		this.leftSRX.setSensorPhase(true);
+    /* OI */
+    private final BionicF310 speedInputGamepad;
+    private final BionicAxis speedInputAxis;
+    private final double speedScaleFactor = 1.0;
+    private final BionicF310 rotationInputGamepad;
+    private final BionicAxis rotationInputAxis;
+    private final double rotationScaleFactor = 1.0;
+
+    /* Sensors */
+    private Gyro bionicGyro;
+    //	private double gyro_p;
+    private PathgenUtil pathgen;
+
+    /* Hardware Initialization */
+
+    /**
+     * @param leftSRX              Left Drivetrain SRX
+     * @param rightSRX             Right Drivetrain SRX
+     * @param speedInputGamepad    Speed Input Gamepad/Joystick
+     * @param speedInputAxis       Speed Input Axis
+     * @param rotationInputGamepad Rotation Input Gamepad/Joystick
+     * @param rotationInputAxis    Rotation Input Axis
+     * @param encoder              Encoder type plugged into SRXs
+     * @param encoder_p            Proportional Constant in Encoder PID Controller
+     * @param encoder_i            Integral Constant in Encoder PID Controller
+     * @param encoder_d            Derivative Constant in Encoder PID Controller
+     * @param bionicGyro           Gyro to Use for Closed-Loop
+     * @param gyro_p               Proportional Constant in Gyro PID Controller
+     * @param maxVelocity          Max Velocity used for Path Generation
+     * @param maxAccel             Max Acceleration used for Path Generation
+     * @param maxJerk              Max Jerk used for Path Generation
+     * @param drivebaseWidth       Drivebase width between center of wheels
+     * @param wheelDiameter        Wheel diameter from end to end
+     */
+    public BionicDrive(BionicSRX leftSRX, BionicSRX rightSRX,
+                       BionicF310 speedInputGamepad, BionicAxis speedInputAxis,
+                       BionicF310 rotationInputGamepad, BionicAxis rotationInputAxis,
+                       FeedbackDevice encoder, double encoder_p, double encoder_i, double encoder_d,
+                       Gyro bionicGyro, double gyro_p,
+                       double maxVelocity, double maxAccel, double maxJerk,
+                       double drivebaseWidth, double wheelDiameter) {
+        super();
+
+        this.leftSRX = leftSRX;
+        this.rightSRX = rightSRX;
+
+        this.leftSRX.enableVoltageCompensation(true);
+        this.rightSRX.enableVoltageCompensation(true);
+
+        this.leftSRX.configSelectedFeedbackSensor(encoder);
+        this.rightSRX.configSelectedFeedbackSensor(encoder);
+
+        this.leftSRX.setSensorPhase(true);
         this.rightSRX.setSensorPhase(true);
-		
-		// Use F of 1023 for percentVBus Feedforward (as found by @oblarg)
-		this.leftSRX.configPIDF(encoder_p, encoder_i, encoder_d, 1023);
-		this.rightSRX.configPIDF(encoder_p, encoder_i, encoder_d, 1023);
-		
-		this.leftSRX.changeMotionControlFramePeriod(profileInterval);
-		this.rightSRX.changeMotionControlFramePeriod(profileInterval);
-		
-		this.bionicGyro = bionicGyro;
+
+        // Use F of 1023 for percentVBus Feedforward (as found by @oblarg)
+        this.leftSRX.configPIDF(encoder_p, encoder_i, encoder_d, 1023);
+        this.rightSRX.configPIDF(encoder_p, encoder_i, encoder_d, 1023);
+
+        this.leftSRX.changeMotionControlFramePeriod(profileInterval);
+        this.rightSRX.changeMotionControlFramePeriod(profileInterval);
+
+        this.bionicGyro = bionicGyro;
 //		this.gyro_p = gyro_p;
-		
-		this.pathgen = new PathgenUtil(new Trajectory.Config(
-				Trajectory.FitMethod.HERMITE_CUBIC,
-				Trajectory.Config.SAMPLES_HIGH,
-				(double) profileInterval / 1000, 
-				maxVelocity, maxAccel, maxJerk),
-				drivebaseWidth, wheelDiameter);
-		
-		this.rotationInputGamepad = rotationInputGamepad;
-		this.rotationInputAxis = rotationInputAxis;
-		
-		this.speedInputGamepad = speedInputGamepad;
-		this.speedInputAxis = speedInputAxis;
-		
-		differentialDrive = new DifferentialDrive(leftSRX, rightSRX);
-	}
-	
-	/**
-	 * @return Returns Robot's Current Heading [0, 360)
-	 */
-	public double getHeading() {
-		return bionicGyro.getAngle();
-	}
-	
-	/* Handle Control Modes */
-	@Override 
-	protected void initDefaultCommand() {}
-	
-	/**
-	 * Called from WPILib Scheduler
-	 */
-	@Override
-	public void periodic() {
-		switch(controlMode) {
-		case MotionProfile:
-			leftSRX.runMotionProfile();
-			rightSRX.runMotionProfile();
-			break;
-		case PercentOutput:
-		default:
-			double speed = speedInputGamepad.getThresholdAxis(speedInputAxis, 0.15) * speedScaleFactor;
-			double rotation = rotationInputGamepad.getThresholdAxis(rotationInputAxis, 0.15) * rotationScaleFactor;
-			
-			differentialDrive.arcadeDrive(speed, rotation, false);
-		}
-	}
-		
-	/**
-	 * @param points Path consisting of waypoints to follow
-	 * @return Returns a Command that can be used by the operator and autonomous CommandGroups.
-	 */
-	public Command driveWaypoints(Waypoint[] points) {
-		return new DriveWaypoints(points);
-	}
-	
-	private class DriveWaypoints extends Command {
-		private final TankTrajectory trajectory; 
-		
-		public DriveWaypoints(Waypoint[] points) {
+
+        this.pathgen = new PathgenUtil(new Trajectory.Config(
+                Trajectory.FitMethod.HERMITE_CUBIC,
+                Trajectory.Config.SAMPLES_HIGH,
+                (double) profileInterval / 1000,
+                maxVelocity, maxAccel, maxJerk),
+                drivebaseWidth, wheelDiameter);
+
+        this.rotationInputGamepad = rotationInputGamepad;
+        this.rotationInputAxis = rotationInputAxis;
+
+        this.speedInputGamepad = speedInputGamepad;
+        this.speedInputAxis = speedInputAxis;
+
+        differentialDrive = new DifferentialDrive(leftSRX, rightSRX);
+    }
+
+    /**
+     * @return Returns Robot's Current Heading [0, 360)
+     */
+    public double getHeading() {
+        return bionicGyro.getAngle();
+    }
+
+    /* Handle Control Modes */
+    @Override
+    protected void initDefaultCommand() {
+    }
+
+    /**
+     * Called from WPILib Scheduler
+     */
+    @Override
+    public void periodic() {
+        switch (controlMode) {
+            case MotionProfile:
+                leftSRX.runMotionProfile();
+                rightSRX.runMotionProfile();
+                break;
+            case PercentOutput:
+            default:
+                double speed = speedInputGamepad.getThresholdAxis(speedInputAxis, 0.15) * speedScaleFactor;
+                double rotation = rotationInputGamepad.getThresholdAxis(rotationInputAxis, 0.15) * rotationScaleFactor;
+
+                differentialDrive.arcadeDrive(speed, rotation, false);
+        }
+    }
+
+    /**
+     * @param points Path consisting of waypoints to follow
+     * @return Returns a Command that can be used by the operator and autonomous CommandGroups.
+     */
+    public Command driveWaypoints(Waypoint[] points) {
+        return new DriveWaypoints(points);
+    }
+
+    private class DriveWaypoints extends Command {
+        private final TankTrajectory trajectory;
+
+        public DriveWaypoints(Waypoint[] points) {
             DriverStation.reportWarning("Generating Path", false);
             trajectory = pathgen.getTrajectory(points);
 
-			setInterruptible(false);
-		}
+            setInterruptible(false);
+        }
 
-		@Override
-		protected void initialize(){
+        @Override
+        protected void initialize() {
             rightSRX.setInverted(!rightSRX.getInverted());
 
             leftSRX.setNeutralMode(NeutralMode.Brake);
@@ -178,20 +180,20 @@ public class BionicDrive extends Subsystem {
         }
 
         @Override
-		protected boolean isFinished() {
-			return leftSRX.isMotionProfileFinished() && rightSRX.isMotionProfileFinished();
-		}
-		
-		@Override
-		protected void end() {
-			rightSRX.setInverted(!rightSRX.getInverted());
+        protected boolean isFinished() {
+            return leftSRX.isMotionProfileFinished() && rightSRX.isMotionProfileFinished();
+        }
+
+        @Override
+        protected void end() {
+            rightSRX.setInverted(!rightSRX.getInverted());
 
             leftSRX.setNeutralMode(NeutralMode.Coast);
             rightSRX.setNeutralMode(NeutralMode.Coast);
 
             differentialDrive.setSafetyEnabled(true);
-		    controlMode = DriveMode.PercentOutput;
-		}
-	}
+            controlMode = DriveMode.PercentOutput;
+        }
+    }
 
 }
