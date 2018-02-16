e8d17f313cba6ef6c98a66262953230eef585f88
Roshan Ravi <roshanravi10@gmail.com>
2018-02-15 23:37:32 -0500

    Update NavX Code

--- src/main/java/org/team4909/bionicframework/hardware/sensors/gyro/BionicNavX.java
+++ src/main/java/org/team4909/bionicframework/hardware/sensors/gyro/BionicNavX.java
@@ -1,12 +1,12 @@
 package org.team4909.bionicframework.hardware.sensors.gyro;
 
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 public class BionicNavX extends AHRS implements Gyro {
     public BionicNavX(){
-        super(SerialPort.Port.kMXP);
+        super(SPI.Port.kMXP);
     }
 
     @Override
