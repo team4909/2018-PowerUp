a3d392079e87297c7ddcadf7b128d265275c9e6f
jaspercoughlin <jazzmancoughlin@gmail.com>
2018-02-28 16:11:16 -0500

    Fixed LEDs

--- arduino/SLAVE_I2C/SLAVE_I2C.ino
+++ arduino/SLAVE_I2C/SLAVE_I2C.ino
@@ -1,50 +1,46 @@
 #include <Adafruit_NeoPixel.h>
-#include <Adafruit_NeoMatrix.h>
-#include <Adafruit_GFX.h> 
 #include <Wire.h>
 
-#define PIXEL_PIN    6
-#define PIXEL_COUNT 64
+#define PIN 5
+#define NUM_LEDS 30 
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+  // NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+  // NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+ Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, 5, NEO_GRB + NEO_KHZ800); 
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_b = Adafruit_NeoPixel(30, 5, NEO_GRB + NEO_KHZ800);
-
-int deviceNumber = 4;
 void setup() {
   // Start I2C as slave
-  Wire.begin(deviceNumber);
-  Wire.onReceive(receiveEvent);
-  int arduinoElevatorPosition = 0;
+  Wire.begin(4);
+  // Wire.onReceive(receiveEvent);
+  double arduinoElevatorPosition = 0;
   Serial.begin(9600);
-  // Initialize LED Strip
+  //Initialize LED Strip
   strip.begin();
-      setColor(148, 0, 211);
+  strip.setBrightness(255);
   strip.show();
-}
+  }
+  
 
-void receiveEvent(int bytes) {
-  // Read the last number sent (0-30)
-  // int signalStatus = Wire.read();
-  int signalStatus = 29;
-  if (signalStatus <= 30){
-    for(int i = 0; i >= 29-signalStatus; i++){
-          strip.setPixelColor(i, 215, 40, 50);
-    }
-    for(int i = 0; i <= signalStatus; i++){
-          strip.setPixelColor(i, 40, 215, 160);
-    }   
+int n = 1;
+
+void loop() {
+  
+double arduinoElevatorPosition = Wire.read();
+ if(arduinoElevatorPosition >= n){
+ for(int i = 0; i < n; i++){
+
+    strip.setPixelColor(i, 255, 0, 0);
     strip.show();
+  //Sets pixel color 
   }
-  
-  Serial.println(signalStatus);
-}
-
-void loop() {}
-
-void setColor(int red, int green, int blue) {
-  for(int i = 0; i < PIXEL_COUNT; i++){
-    strip.setPixelColor(i, red, green, blue);
+  //Sets pixel color 
   }
-  
-  strip.show();
+
+  if(arduinoElevatorPosition >= n + 1){
+  n++;
+  }
 }
