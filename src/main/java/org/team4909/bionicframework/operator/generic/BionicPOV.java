5e4276d976ca6eab3975082a390596693320bae4
Roshan Ravi <roshanravi10@gmail.com>
2018-03-01 08:36:55 -0500

    Base for POV

---
+++ src/main/java/org/team4909/bionicframework/operator/generic/BionicPOV.java
@@ -0,0 +1,13 @@
+package org.team4909.bionicframework.operator.generic;
+
+public class BionicPOV {
+    private int povAngle;
+
+    public BionicPOV(int angle) {
+        povAngle = angle;
+    }
+
+    public int getNumber(){
+        return povAngle;
+    }
+}
