69a2ac403036b733961668b0430c697ac5ac4353
jaspercoughlin <jazzmancoughlin@gmail.com>
2018-03-02 16:13:57 -0500

    Rainbow Animation
    rainbow arduino animation flor the neopixel.

--- arduino/SLAVE_I2C/SLAVE_I2C.ino
+++ arduino/SLAVE_I2C/SLAVE_I2C.ino
@@ -11,11 +11,32 @@
   // NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
  Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, 5, NEO_GRB + NEO_KHZ800); 
-
+/*
+int r = 1;
+int g = 2;
+int b = 3;
+void receiveEvent(int bytes){
+  // Read the last number sent (0-30)
+  // int signalStatus = Wire.read();
+  int signalStatus = 16;
+  // Scale signalStatus to 0 - 29
+  if (signalStatus <= 29){
+    for(int i = 0; i >= 29-signalStatus; i++){
+          strip.setPixelColor(i, strip.Color(215, 40, 50));
+          strip.show();
+    }
+    for(int i = 0; i <= signalStatus; i++){
+          strip.setPixelColor(i, strip.Color(40, 215, 160));
+          strip.show();
+    }   
+  }
+  Serial.println(signalStatus); 
+}
+*/
 void setup() {
   // Start I2C as slave
   Wire.begin(4);
-  // Wire.onReceive(receiveEvent);
+ //  Wire.onReceive(receiveEvent);
   double arduinoElevatorPosition = 0;
   Serial.begin(9600);
   //Initialize LED Strip
@@ -23,17 +44,15 @@ void setup() {
   strip.setBrightness(255);
   strip.show();
   }
-  
-
-int n = 1;
+int n = 1;  
 
 void loop() {
-  
+
 double arduinoElevatorPosition = Wire.read();
  if(arduinoElevatorPosition >= n){
  for(int i = 0; i < n; i++){
 
-    strip.setPixelColor(i, 255, 0, 0);
+    strip.setPixelColor(i, 0, 255, 0);
     strip.show();
   //Sets pixel color 
   }
@@ -43,4 +62,5 @@ double arduinoElevatorPosition = Wire.read();
   if(arduinoElevatorPosition >= n + 1){
   n++;
   }
+
 }
---
+++ arduino/SLAVE_I2C/rainbow/rainbow.ino
@@ -0,0 +1,62 @@
+#include <Adafruit_NeoPixel.h>
+//#ifdef __AVR__
+//#include <avr/power.h>
+//#endif
+
+#define PIN 5
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, 5, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  //#if defined (__AVR_ATtiny85__)
+   // if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  //#endif
+  // End of trinket special code
+
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  delay(1000);
+  strip.setBrightness(125);
+}
+void loop() {
+  rainbowCycle(0.7);
+}
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
