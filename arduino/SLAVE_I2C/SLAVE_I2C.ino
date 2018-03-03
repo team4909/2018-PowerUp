10b10fe2573c5d68a6146c7c4b1512f729323ccd
aamonkar17 <36334490+aamonkar17@users.noreply.github.com>
2018-03-02 19:14:57 -0500

    Added more animations

--- arduino/SLAVE_I2C/SLAVE_I2C.ino
+++ arduino/SLAVE_I2C/SLAVE_I2C.ino
@@ -15,6 +15,7 @@ void setup() {
   Serial.begin(9600);
   // Initialize LED Strip
   strip.begin();
+  strip.setBrightness(255);
 //    setColor(148, 0, 211);
   strip.show();
 }
@@ -22,7 +23,7 @@ void setup() {
 void receiveEvent() {
   // Read the last number sent (0-30)
   // int LED = Wire.read();
-  int LED = 1;
+  //int LED = 1;
 }
 
 int rainbow1(int wait){
@@ -94,12 +95,117 @@ uint32_t Wheel(byte WheelPos) {
   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
+void Fire(int Cooling, int Sparking, int SpeedDelay) {
+  static byte heat[30]; 
+  int cooldown; 
+  
+  // Step 1.  Cool down every cell a little
+  for( int i = 0; i < 30; i++) {
+    cooldown = random(0, ((Cooling * 10) / 30) + 2); 
+    
+    if(cooldown>heat[i]) {
+      heat[i]=0;
+    } else {
+      heat[i]=heat[i]-cooldown;
+    }
+  }
+  
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for( int k= 30 - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+  }
+    
+  // Step 3.  Randomly ignite new 'sparks' near the bottom
+  if( random(255) < Sparking ) {
+    int y = random(7);
+    heat[y] = heat[y] + random(160,255);
+    //heat[y] = random(160,255);
+  }
+
+  // Step 4.  Convert heat to LED colors  
+  for( int j = 0; j < 30; j++) {
+    setPixelHeatColor(j, heat[j] );
+  }
+
+  showStrip();
+  delay(SpeedDelay);
+}
+
+void setPixelHeatColor (int Pixel, byte temperature) {
+  // Scale 'heat' down from 0-255 to 0-191
+  byte t192 = round((temperature/255.0)*191);
+ 
+  // calculate ramp up from
+  byte heatramp = t192 & 0x3F; // 0..63
+  heatramp <<= 2; // scale up to 0..252
+ 
+  // figure out which third of the spectrum we're in:
+  if( t192 > 0x80) {                     // hottest
+    setPixel(Pixel, 255, 255, heatramp);
+  } else if( t192 > 0x40 ) {             // middle
+    setPixel(Pixel, 255, heatramp, 0);
+  } else {                               // coolest
+    setPixel(Pixel, heatramp, 0, 0);
+  }
+}
+
+
+void showStrip() {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.show();
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   FastLED.show();
+ #endif
+}
+
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H 
+   // FastLED
+   leds[Pixel].r = red;
+   leds[Pixel].g = green;
+   leds[Pixel].b = blue;
+ #endif
+}
+
+void greenWhite(){
+  for(int i = 0; i <= 40; i++){
+    int i1 = i-1;
+    int i2 = i;
+    int i3 = i+1; 
+    strip.setPixelColor(i1, 0, 255, 0);
+    strip.setPixelColor(i2, 0, 255, 0);
+    strip.setPixelColor(i3, 0, 255, 0);
+    
+    int a1 = i+6;
+    int b1 = i-6;
+
+    strip.setPixelColor(a1, 255, 255, 255);
+    strip.setPixelColor(b1, 255, 255, 255);
+    strip.show();
+     
+    delay(100);
+  };
+}
+
 void loop() {
-  int LED = 2;
-  if (LED = 1){
+  int LED = 3;
+  if (LED == 1){
     rainbow1(25); 
   }
-  if (LED = 2){
+  else if (LED == 2){
     rainbow2(0.7);
+  }
+  else if (LED == 3){
+    Fire(55,120,15);
+  }
+  else if (LED == 4){
+    greenWhite();
   }
 }
