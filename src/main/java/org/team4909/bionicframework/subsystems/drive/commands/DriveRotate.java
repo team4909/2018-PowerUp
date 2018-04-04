fe5f4e6565ee568044050b5055d1ff3f7459cbcc
Team4909 <team4909@gmail.com>
2018-04-04 15:34:00 -0400

    Updates

--- src/main/java/org/team4909/bionicframework/subsystems/drive/commands/DriveRotate.java
+++ src/main/java/org/team4909/bionicframework/subsystems/drive/commands/DriveRotate.java
@@ -19,6 +19,8 @@ public class DriveRotate extends PIDCommand {
         this.leftSRX = leftSRX;
         this.rightSRX = rightSRX;
 
+        angle = -angle;
+
         if(angle<0){
             direction = -1;
         }else{
